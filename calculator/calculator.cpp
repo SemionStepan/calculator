7e5e4a34ebcb2567be9522395cd29b4a3add7e1d
SemionStepan <95352195+SemionStepan@users.noreply.github.com>
2023-09-18 19:20:57 +0300

    Added exec

--- calculator/calculator.cpp
+++ calculator/calculator.cpp
@@ -38,17 +38,55 @@ Programm::Programm(int chase)
     _chase = chase;
 }
 
-void Programm::hello()
-{
-    cout << "Здравствуйте, вы используете калькулятор, введите первое число:" << endl;
-}
-
-void Programm::input()
-{
-    cin >> calc.first;
-}
-
 void Programm::exec()
 {
-
+    while (_chase != 7)
+    {
+        int first, second = 0;
+        switch(_chase)
+        {
+            case 1:
+                cout << "Введите первое число" << endl;
+                cin >> first;
+                cout << "Введите второе число" << endl;
+                cin >> second;
+                cout << calc.plus(first, second) << endl;
+            break;
+            case 2:
+                cout << "Введите первое число" << endl;
+                cin >> first;
+                cout << "Введите второе число" << endl;
+                cin >> second;
+                cout << calc.minus(first, second) << endl;
+            break;
+            case 3:
+                cout << "Введите первое число" << endl;
+                cin >> first;
+                cout << "Введите второе число" << endl;
+                cin >> second;
+                cout << calc.multiply(first, second) << endl;
+            break;
+            case 4:
+                cout << "Введите первое число" << endl;
+                cin >> first;
+                cout << "Введите второе число" << endl;
+                cin >> second;
+                cout << calc.division(first, second) << endl;
+            break;
+            case 5:
+                cout << "Введите первое число(основание)" << endl;
+                cin >> first;
+                cout << "Введите второе число(степень)" << endl;
+                cin >> second;
+                cout << calc.exponentiation(first, second) << endl;
+            break;
+            case 6:
+                cout << "Введите первое число" << endl;
+                cin >> first;
+                cout << "Введите второе число" << endl;
+                cin >> second;
+                cout << calc.log(first, second) << endl;
+            break;
+        }
+    }
 }
